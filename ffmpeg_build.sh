882dc9131c80711292a0b36980091d9f1669e349
shengshanbai <shengshanbai@126.com>
2017-12-27 08:19:30 +0800

    初始化和编译最新ffmpeg

---
+++ ffmpeg_build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. abi_settings.sh $1 $2 $3
+
+pushd ffmpeg
+
+case $1 in
+  armeabi-v7a | armeabi-v7a-neon)
+    CPU='cortex-a8'
+  ;;
+  x86)
+    CPU='i686'
+  ;;
+esac
+
+make clean
+
+./configure \
+--target-os="$TARGET_OS" \
+--cross-prefix="$CROSS_PREFIX" \
+--arch="$NDK_ABI" \
+--cpu="$CPU" \
+--enable-runtime-cpudetect \
+--sysroot="$NDK_SYSROOT" \
+--enable-pic \
+--enable-pthreads \
+--disable-debug \
+--disable-ffserver \
+--enable-version3 \
+--enable-hardcoded-tables \
+--enable-gpl \
+--disable-doc \
+--enable-shared \
+--disable-static \
+--disable-linux-perf \
+--prefix="${2}/build/${1}" \
+--extra-cflags="-I${TOOLCHAIN_PREFIX}/include $CFLAGS" \
+--extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
+--extra-cxxflags="$CXX_FLAGS" || exit 1
+
+make -j${NUMBER_OF_CORES} && make install || exit 1
+
+popd
